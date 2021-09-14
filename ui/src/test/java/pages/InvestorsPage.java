874384acb212cd3a9bec8c525c3b70c6d54dbbbc
OLEKSANDR HOLOVATYI <oleksandr_holovatyi@epam.com>
2021-09-15 00:39:42 +0300

    [JIRA-OOO4] Resolved pull request issue 1: - moved common methods to new abstract class "CommonPage" and extended other pages form the class.

--- ui/src/test/java/pages/InvestorsPage.java
+++ ui/src/test/java/pages/InvestorsPage.java
@@ -3,7 +3,6 @@ package pages;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.WebDriverRunner;
 import io.cucumber.datatable.DataTable;
 
 import java.util.ArrayList;
@@ -12,15 +11,11 @@ import java.util.List;
 import static com.codeborne.selenide.Selenide.$$x;
 import static com.codeborne.selenide.Selenide.$x;
 
-public class InvestorsPage {
+public class InvestorsPage extends CommonPage {
 
     String listOfLinksActualXpath = ".//section//ul[contains(@class,'list')]/*";
     SelenideElement listOfLinksActualElement = $x(listOfLinksActualXpath);
     ElementsCollection listOfLinksActualCollection = $$x(listOfLinksActualXpath);
-
-    public boolean isUrlCorrect(String url) {
-        return WebDriverRunner.getWebDriver().getCurrentUrl().equals(url);
-    }
 
     public boolean isListOfLinksVisible() {
         return listOfLinksActualElement.should(Condition.appear).is(Condition.visible);
--- ui/src/test/java/pages/MainPage.java
+++ ui/src/test/java/pages/MainPage.java
@@ -1,44 +1,6 @@
 package pages;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
-
-
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$x;
-
 //TODO refactor extend main page, move not common things in separate classes
-public class MainPage {
+public class MainPage extends CommonPage {
 
-    SelenideElement locationButton = $(".location-selector__button");
-    SelenideElement locationsList = $(".location-selector__list");
-    SelenideElement investorsButton = $x(".//ul[@class='footer__links']//a[text()='Investors']");
-    SelenideElement cookiesBannerButton = $x("//button[@aria-label='Accept our use of cookies']/span[text()='Accept']");
-
-    public void closeCookiesBanner() {
-        if (cookiesBannerButton.isDisplayed()) cookiesBannerButton.click();
-    }
-
-    public void openLocationMenu() {
-        locationButton.click();
-    }
-
-    public boolean isMenuVisible() {
-        return locationsList.should(Condition.appear).is(Condition.visible);
-    }
-
-    public boolean isLocationCorrect(String location) {
-        return locationButton.should(Condition.appear).getText().equals(location);
-    }
-
-    public void activateLocation(String region, String lang) {
-        SelenideElement LocationButton = $x(".//ul[@class='location-selector__list']//a[text()='" + region + " ']");
-        String location = region + " (" + lang + ")";
-        if (!isLocationCorrect(location)) LocationButton.click();
-    }
-
-    public void openInvestorsPage() {
-        closeCookiesBanner();
-        investorsButton.click();
-    }
 }
